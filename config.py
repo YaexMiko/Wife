cf5d28824315be2cfd78c2deddfc5c4726742177
YaexMiko <aloksinghmains756@gmail.com>
2024-11-23 13:28:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ BOT_STATS_TEXT = os.environ.get("BOTS_STATS_TEXT","<b>𝙱𝙾𝚃 𝚄𝙿𝚃�
 USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "❌ 𝙿𝚕𝚎𝚊𝚜𝚎 𝙰𝚟𝚘𝚒𝚍 𝙳𝚒𝚛𝚎𝚌𝚝 𝙼𝚎𝚜𝚜𝚊𝚐𝚎𝚜. 𝙸'𝚖 𝚆𝚘𝚛𝚔𝚒𝚗𝚐 𝙵𝚘𝚛 𝙾𝚗𝚕𝚢  @Team_Originals")
 
 #your bot token here from https://telegram.me/BotFather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7462770153:AAHK8UhmjaT92xDcv3z4efzfE-uV_o0ZEBQ") 
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7462770153:AAHz3IifHdMO6xtfo8A6S0T36UbvsE7minY") 
 #your api id from https://my.telegram.org/apps
 APP_ID = int(os.environ.get("APP_ID", "20071888"))
 #your api hash from https://my.telegram.org/apps
